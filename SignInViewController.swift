8f07e9fae3ea37cad004a135c14834a36d9c6ab3
Chris Chang <chris@Changui-MacBook-Air.local>
2018-02-20 22:21:27 +0900

    change background to transparent

---
+++ SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  LoginPopup
+//
+//  Created by Chris Chang on 20/02/2018.
+//  Copyright © 2018 Chris Chang. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  LoginPopup
+//
+//  Created by Chris Chang on 20/02/2018.
+//  Copyright © 2018 Chris Chang. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
